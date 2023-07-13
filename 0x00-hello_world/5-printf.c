4daba40748f97798b346eeaf0ae7d7d885a7aeb2
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2023-07-13 09:05:09 -0700

    Betty check on 5-printf.c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program is printing a statement using printf() instead of puts()
+ * Return: 0 (Program executed successfully)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
