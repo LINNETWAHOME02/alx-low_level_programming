1c1374db319d42f65e3846307eaa68dac70e42e1
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2023-07-13 08:59:36 -0700

    Betty test c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program prints a statement using put() instead of printf()
+ * Return: 0 (Program executed successfully)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
