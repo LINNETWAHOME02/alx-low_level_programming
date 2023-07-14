03c84f8a6ef0034eb0c6a5e6f75e2f7cc1254162
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2023-07-14 11:35:42 -0700

    diff combos

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,18 +9,21 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 48; num1 <= 57; num1++)
+	for (num1 = '0'; num1 <= '9'; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 57; num2++)
+		for (num2 = num1 + 1; num2 <= '9'; num2++)
 		{
-			putchar(num1);
-			putchar(num2);
-			if (num1 == 57 &&  num2 == 57)
+			if (num1 != num2)
 			{
-				continue;
+				putchar(num1);
+				putchar(num2);
+				if (num1 == '8' &&  num2 == '9')
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
 			}
-			putchar(',');
-			putchar(' ');
 		}
 	}
 	putchar('\n');
