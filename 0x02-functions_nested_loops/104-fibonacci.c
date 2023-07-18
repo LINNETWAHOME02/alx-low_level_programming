eeecaaeda446f04f848beee372109622a02ebb5c
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2023-07-18 15:21:28 -0700

    Fibonacci 3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,45 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - first 98 Fibonacci numbers, starting
- *  with 1 and 2, followed by a new line
+ * main - Prints the first 98 Fibonacci numbers
  *
- *  Return: 0 (Code success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int counter;
-	unsigned long f1 = 0, f2 = 1, sum;
-	unsigned long f1_h1, f1_h2, f2_h1, f2_h2;
-	unsigned long h1, h2;
+	unsigned long int fib1 = 1, fib2 = 2, fibonacci;
+	int count;
 
-	for (counter = 0; counter < 92; counter++)
+	printf("%lu, %lu", fib1, fib2);
+	for (count = 3; count <= 98; count++)
 	{
-		sum = f1 + f2;
-		printf("%lu, ", sum);
-		f1 = f2;
-		f2 = sum;
-	}
-	f1_h1 = f1 / 1000000000;
-	f2_h1 = f2 / 1000000000;
-	f1_h2 = f1 % 1000000000;
-	f2_h2 = f2 % 1000000000;
-	for (counter = 93; counter < 99; counter++)
-	{
-		h1 = f1_h1 + f2_h1;
-		h2 = f1_h2 + f2_h2;
-		if (f1_h2 + f2_h2 > 9999999999)
-		{
-			h1 += 1;
-			h2 %= 10000000000;
-		}
-		printf("%lu%lu", h1, h2);
-		if (counter != 98)
-			printf(",");
-		f1_h1 = f2_h1;
-		f1_h2 = f2_h2;
-		f2_h1 = h1;
-		f2_h2 = h2;
+		fibonacci = fib1 + fib2;
+		printf(", %lu", fibonacci);
+		fib1 = fib2;
+		fib2 = fibonacci;
 	}
 	printf("\n");
 	return (0);
