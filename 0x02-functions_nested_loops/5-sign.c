9861b4b8048a752d51ef942b902bd9161d61227a
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2023-07-18 04:06:47 -0700

    Sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - Print the sign of a number
+ * @n: Number to be checked
+ * Return: 1 if greater than zero or 0 if zero or -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	} else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	} else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
