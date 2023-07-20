0627bd1a9a3430bd1f9aa529a09dc295580b6c49
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2023-07-20 14:54:46 -0700

    task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,7 +4,6 @@
 /**
  * main - Finding and prints the largest prime factor of the number 612852475143
  * followed by a new line
- *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -15,13 +14,11 @@ int main(void)
 
 	n = 612852475143;
 	max = -1;
-
 	while (n % 2 == 0)
 	{
 		max = 2;
 		n /= 2;
 	}
-
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
@@ -30,11 +27,8 @@ int main(void)
 			n = n / i;
 		}
 	}
-
 	if (n > 2)
 		max = n;
-
 	printf("%ld\n", max);
-
 	return (0);
 }
