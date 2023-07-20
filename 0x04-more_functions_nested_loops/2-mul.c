eb25b5aff8c4ba4e6f8e72ca25e2217750f948c1
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2023-07-20 13:58:47 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Multiples of 2 integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: multiplicattion
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
