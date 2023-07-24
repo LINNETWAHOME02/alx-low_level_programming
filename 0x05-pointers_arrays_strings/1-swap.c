1fc182bb2e34a76aa335673c5c1d806a57f25795
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2023-07-24 11:54:11 -0700

    Swapping numbers using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - Swapping the values of two integers
+ *
+ * @a: First integer to be swapped
+ * @b: Second integer to be swapped
+ *
+ * Return: Nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
