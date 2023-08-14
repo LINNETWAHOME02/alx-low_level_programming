5f0127654b71ba2f2801b7e6a7ae39f6687f06d7
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2023-08-14 13:15:15 -0700

    Structures in C task 1 - struct dog and void init_dog()

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -18,10 +18,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 	{
-		printf("Error: pointer cannot be NULL/empty\n");
-		return;
+		printf("Error: pointer cannot be NULL/empty.\n");
 	}
 	(*d).name = name;
-	d -> age = age;
-	d -> owner = owner;
+	d->age = age;
+	d->owner = owner;
 }
