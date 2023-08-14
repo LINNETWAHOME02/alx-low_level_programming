dba85716eb61c8dd5d908a251450f069f50d01b5
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2023-08-14 13:58:32 -0700

    Structures in C task 3 - struct dog and dog_t using typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,15 +7,19 @@
  * @age: The dog's age
  * @owner: Pointer to the dog owner's name
  *
- * dog_t - typedef for struct dog
- *
  */
-typedef struct dog
+struct dog
 {
         char *name;
         float age;
         char *owner;
-} dog_t;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *this_dog(char *name, float age, char *owner);
