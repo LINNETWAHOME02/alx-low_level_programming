9a99ce06ef6d76d6e5a518788ae795ff8b517c6d
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2023-08-14 12:33:54 -0700

    Structures in C task 0 - struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Structure describing a dog's qualities
+ * @name: Pointer to the dog's name
+ * @age: The dog's age
+ * @owner: Pointer to the dog owner's name
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
