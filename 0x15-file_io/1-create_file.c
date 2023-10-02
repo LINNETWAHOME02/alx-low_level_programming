54cb229edcd7c48ff90e6544f7bc26da544f6559
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2023-10-02 13:24:42 -0700

    File operations

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,23 +8,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_des;
-	ssize_t bytes_written;
-	
+	int fd, w, len = 0;
+
 	if (filename == NULL)
 		return (-1);
-	file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file_des == -1)
-		return (-1);
+
 	if (text_content != NULL)
 	{
-		bytes_written = write(file_des, text_content, _strlen(text_content));
-		if (bytes_written == -1 || bytes_written != _strlen(text_content))
-		{
-			close(file_des);
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	close(file_des);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
 	return (1);
 }
