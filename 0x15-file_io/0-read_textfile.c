44679174f79fa74082d937e2e379a3914710e0d1
LINNETWAHOME02 <nyokabilinnet@gmail.com>
2023-10-02 12:48:52 -0700

    File operations

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,26 +15,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	
 	if (filename == NULL)
 		return (0);
-
+	/*Create buffer and open file*/
 	buffer = (char *)malloc(letters);
 	file_des = open(filename, O_RDONLY);
 	if (file_des == -1)
 		return (0);
-
+	/*Reading file*/
 	bytes_read = read(file_des, buffer, letters);
 	if (bytes_read == -1) {
 		close(file_des);
 		free(buffer);
 		return (0);
 	}
-
+	/*Writting text to standard output*/
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read) {
 		close(file_des);
 		free(buffer);
 		return (0);
 	}
-
+	/*Closing the file and freeing the buffer*/
 	close(file_des);
 	free(buffer);
 	return (bytes_written);
